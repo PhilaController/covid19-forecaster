531d23a1c3b5455f50ef87f91f1165cc83f61366
Nick Hand <nicholas.adam.hand@gmail.com>
2020-06-22 23:22:47 -0400

    update data through May 2020

--- covid19_forecaster/data.py
+++ covid19_forecaster/data.py
@@ -118,7 +118,7 @@ def load_data_by_sector(kind: str, main_sectors_only=False) -> pd.DataFrame:
         raise ValueError(f"Allowed values are: {allowed}")
 
     # Load the data
-    path = DATA_DIR / "revenue-reports" / f"{kind}_by_sector.csv"
+    path = DATA_DIR / "revenue-reports" / f"{kind}-by-sector.csv"
     out = pd.read_csv(path)
 
     if main_sectors_only:
@@ -127,49 +127,29 @@ def load_data_by_sector(kind: str, main_sectors_only=False) -> pd.DataFrame:
     return out
 
 
-def load_monthly_collections(kind: str) -> pd.DataFrame:
+def load_monthly_collections() -> pd.DataFrame:
     """
-    Load data for monthly collections
-
-    Parameter
-    ----------
-    kind : str
-        load collections for tax, non-tax, or other govts
+    Load data for monthly tax revenue collections
     
     Returns
     -------
     DataFrame :
         the data for collections by month
     """
-    allowed = ["Tax", "Non-Tax", "Other Govts"]
-    if kind not in allowed:
-        raise ValueError(f"Allowed values are: {allowed}")
-
     # Load
-    tag = "_".join([w.lower() for w in kind.replace("-", " ").split()])
-    path = DATA_DIR / "revenue-reports" / f"monthly-collections_{tag}.csv"
+    path = DATA_DIR / "revenue-reports" / "monthly-collections-tax.csv"
     out = pd.read_csv(path)
 
-    # Combine Wage + Earnings
-    if kind == "Tax":
-        out = pd.concat(
-            [
-                out,
-                out.query("name in ['wage', 'earnings']")
-                .groupby(
-                    [
-                        "month",
-                        "year",
-                        "fiscal_year",
-                        "month_name",
-                        "fiscal_month",
-                    ]
-                )["total"]
-                .sum()
-                .reset_index()
-                .assign(name="wage_earnings"),
-            ],
-            axis=0,
-        ).sort_values(["fiscal_year", "fiscal_month"])
-
-    return out
+    return pd.concat(
+        [
+            out,
+            out.query("name in ['wage', 'earnings']")
+            .groupby(
+                ["month", "year", "fiscal_year", "month_name", "fiscal_month"]
+            )["total"]
+            .sum()
+            .reset_index()
+            .assign(name="wage_earnings"),
+        ],
+        axis=0,
+    ).sort_values(["fiscal_year", "fiscal_month"])
