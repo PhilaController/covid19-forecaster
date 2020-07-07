850d6c7446c89a2a6403a7b54a4c50f97ffdcca1
Nick Hand <nicholas.adam.hand@gmail.com>
2020-07-07 11:51:50 -0400

    add fresh option

--- run_forecasts.py
+++ run_forecasts.py
@@ -1,9 +1,18 @@
-from covid19_forecaster.taxes import run_forecasts
+import argparse
 from pathlib import Path
+
+from covid19_forecaster.taxes import run_forecasts
 
 current_directory = Path(__file__).parent
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fresh", action="store_true")
+    args = parser.parse_args()
+
     # Run all of the forecasts and save to file
-    run_forecasts(current_directory / "COVID Budget Impact Analysis.xlsx")
+    run_forecasts(
+        current_directory / "COVID Budget Impact Analysis.xlsx",
+        fresh=args.fresh,
+    )
