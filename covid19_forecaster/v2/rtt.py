a0b7635f301ad3c574c1095c11ab5fb74f84dfea
Nick Hand <nicholas.adam.hand@gmail.com>
2021-03-18 17:30:43 -0400

    add rtt model with data spreadsheet

--- covid19_forecaster/v2/rtt.py
+++ covid19_forecaster/v2/rtt.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 
+from .. import DATA_DIR
 from ..core import RevenueForecast
 from ..forecasters import NoBaselineForecasterBySector
 from . import (
@@ -14,46 +16,31 @@ from . import (
 class RealtyTransferTaxForecast(NoBaselineForecasterBySector, RevenueForecast):
     """Realty transfer tax revenue forecast."""
 
-    ASSUMPTIONS = {
-        "moderate": {
-            "Residential": [
-                59504922.23634641,
-                68170250.75816274,
-                73058693.95655346,
-                66892750.42746627,
-                67800043.69149524,
-                77673338.7114734,
-            ],
-            "Non-Residential": [
-                16430490.674820002,
-                17094282.49808273,
-                17610529.82952483,
-                18142367.830376476,
-                18507029.423767045,
-                18879020.715184763,
-            ],
-        },
-        "severe": {
-            "Residential": [
-                52458286.70835801,
-                59213671.75955736,
-                66204647.623281255,
-                59725736.97083543,
-                59811270.67657426,
-                67513545.93516353,
-            ],
-            "Non-Residential": [
-                13016777.078304004,
-                13542654.872267488,
-                14227913.208804224,
-                14947845.61716972,
-                15399270.554808244,
-                15864328.525563456,
-            ],
-        },
-    }
+    ASSUMPTIONS = {}
 
     def __init__(self, fresh=False):
+
+        # Load the assumptions
+        path = DATA_DIR / "models" / "v2" / "RTT Analysis.xlsx"
+        skiprows = {"moderate": 5, "severe": 15}
+        for scenario in ["moderate", "severe"]:
+
+            # Load the data
+            data = pd.read_excel(
+                path,
+                nrows=2,
+                header=0,
+                index_col=0,
+                usecols="A,D:I",
+                sheet_name="FY21-FY22 RTT Scenarios",
+                skiprows=skiprows[scenario],
+            )
+
+            # Convert to keys --> sectors and values --> revenue
+            self.ASSUMPTIONS[scenario] = {}
+            for k in data.index:
+                self.ASSUMPTIONS[scenario][k] = data.loc[k].tolist()
+
         super().__init__(
             tax_name="rtt",
             forecast_start=FORECAST_START,
